f207e6de8f7ceb119946af4ad4e31477a03e27d7
Amangurjar1507 <Amangurjar1507@gmail.com>
2024-02-13 08:42:40 +0530

    New code

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import {LogBox} from 'react-native';
 import React, {useEffect} from 'react';
 import Route from './src/navigation';
+import 'react-native-gesture-handler';
 import SplashScreen from 'react-native-splash-screen';
 import store from './src/services/redux/store';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
-
+// react-native-inappbrowser-reborn
 LogBox.ignoreLogs([
   "[react-native-gesture-handler] Seems like you're using an old API with gesture components, check out new Gestures system!",
 ]);
--- src/navigation/stacks/authStack.tsx
+++ src/navigation/stacks/authStack.tsx
@@ -1,23 +1,130 @@
-import React, {useEffect} from 'react';
+import React, {useRef, useState} from 'react';
 import {
-  createStackNavigator,
-  StackNavigationProp,
-} from '@react-navigation/stack';
-import screenName from '../screenName';
-import Login from '../../screens/auth/login/Login';
-export type AuthParams = {
-  Login: undefined;
-};
-const Stack = createStackNavigator();
-export type AuthNavigationProps = StackNavigationProp<AuthParams>;
-const AuthStack = () => {
+  StyleSheet,
+  View,
+  PanResponder,
+  Text,
+  Animated,
+  Dimensions,
+} from 'react-native';
+
+const Slider = () => {
+  const [sliderDimensions, setSliderDimensions] = useState({
+    height: null,
+    top: null,
+    bottom: null,
+  });
+
+  const numBoxes = 4;
+  const boxColors = ['#FF5733', '#33FF57', '#B733FF', '#4D94FF'];
+  const boxHeight = sliderDimensions.height / numBoxes;
+  const [showValue, setShowValue] = useState(0);
+  const stepperAnim = useRef(new Animated.Value(0)).current;
+
+  const stepperResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderMove: (_, {dy, moveY}) => {
+      if (moveY > sliderDimensions.top && moveY < sliderDimensions.bottom) {
+        stepperAnim.setValue(dy);
+
+        const currentPosition = moveY - sliderDimensions.top;
+        const index = Math.floor(currentPosition / boxHeight);
+      }
+    },
+  });
+
+  const handleLayout = ({
+    nativeEvent: {
+      layout: {height},
+    },
+  }) => {
+    setSliderDimensions({
+      height,
+      top: 120,
+      bottom: 20 + height,
+    });
+  };
+
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <Stack.Screen name={screenName.login} component={Login} />
-    </Stack.Navigator>
+    <View style={styles.container}>
+      <View style={styles.slider} onLayout={handleLayout}>
+        <View style={styles.rail}>
+          <View style={styles.railFill}>
+            {sliderDimensions.height &&
+              boxColors.map((color, index) => (
+                <View
+                  key={index}
+                  style={[
+                    styles.railFillSpace,
+                    {
+                      height: boxHeight,
+                      backgroundColor: color,
+                    },
+                  ]}
+                />
+              ))}
+          </View>
+        </View>
+        <View style={styles.stepperContainer}>
+          <Animated.View
+            {...stepperResponder.panHandlers}
+            style={[
+              styles.stepper,
+              {
+                transform: [{translateY: stepperAnim}],
+              },
+            ]}
+          />
+        </View>
+        <Text>{showValue}</Text>
+      </View>
+    </View>
   );
 };
-export default AuthStack;
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    backgroundColor: 'red',
+    height: '66%',
+  },
+  slider: {
+    width: 120,
+    height: '82%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    position: 'relative',
+    marginBottom: 50,
+    alignItems: 'center',
+  },
+  rail: {
+    width: 120,
+    height: '100%',
+    alignItems: 'center',
+    flex: 1,
+  },
+  stepperContainer: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+    alignItems: 'center',
+  },
+  stepper: {
+    width: '45%',
+    height: 25,
+    backgroundColor: 'white',
+    flex: 1,
+    borderRadius: 7,
+  },
+  railFill: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 0,
+  },
+  railFillSpace: {
+    width: '100%',
+    position: 'relative',
+  },
+});
+
+export default Slider;
