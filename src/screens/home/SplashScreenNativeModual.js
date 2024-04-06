9a573e4e7ecb637ebcf77461d07419a468b55c87
Amangurjar1507 <Amangurjar1507@gmail.com>
2024-04-06 23:51:10 +0530

    Splash screen  stepy in native code

---
+++ src/screens/home/SplashScreenNativeModual.js
@@ -0,0 +1,35 @@
+// package com.episap;
+// import android.os.Bundle;
+
+// import com.facebook.react.ReactActivity;
+// import com.facebook.react.ReactActivityDelegate;
+// import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+// import com.facebook.react.defaults.DefaultReactActivityDelegate;
+// import org.devio.rn.splashscreen.SplashScreen;
+
+// protected void onCreate(Bundle savedInstanceState) {
+//     SplashScreen.show(this,true);  // here
+//     super.onCreate(savedInstanceState);
+//   }
+
+
+// ITERGATE ADD THE CODE IN ANDROIManActivitry
+
+
+// than S/creen code
+// <?xml version="1.0" encoding="utf-8"?>
+// <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+//     android:orientation="vertical"
+//     android:gravity="center"
+//     android:layout_width="fill_parent"
+//     android:layout_height="fill_parent"
+//     android:background="#09090A">
+
+// <ImageView
+//     android:layout_width="132dp"
+//     android:layout_height="55dp"
+//     android:src="@drawable/logo"
+//     />
+   
+// </LinearLayout>
+
