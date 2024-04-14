a4cf2ad66e421def3b9f1496a9eb91b6cca954b2
Amangurjar1507 <Amangurjar1507@gmail.com>
2024-04-14 11:23:23 +0530

    Get cuureentLocation

---
+++ src/screens/home/getCurrentLocation/CurrentLocation.js
@@ -0,0 +1,25 @@
+
+// import GetLocation from 'react-native-get-location';
+
+// const getCurrentLocation = () => {
+//     setLoad(true);
+//     GetLocation.getCurrentPosition({
+//       enableHighAccuracy: false,
+//       timeout: 5000,
+//     })
+//       .then(location => {
+//         fetchAddressFromGoogle(
+//           location.latitude,
+//           location.longitude,
+//           'current',
+//           '',
+//         );
+//       })
+//       .catch(error => {
+//         ToastMessage({
+//           message: languageData?.data?.location_permission,
+//           type: 'fail',
+//         });
+//         setLoad(false);
+//       });
+//   };
